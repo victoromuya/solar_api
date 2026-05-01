fe1ae066a50578cd9e20ff4ee9e146d3bd64ae69
unknown <viczik16@gmail.com>
2026-05-01 13:10:35 +0100

    host

--- solar_api/settings.py
+++ solar_api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-m73sp69!tij8qjg0dg$_e_equ%z5=tnn&lc&3b70()4c3n$yu!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
